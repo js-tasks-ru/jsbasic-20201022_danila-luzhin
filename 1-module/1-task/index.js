72337f2af274eb03fee80f209507801e6ec20755
Danila Luzhin <DLuzhin@nvg.ru>
2020-11-04 21:35:54 +0300

    0.1task | 1.1task | 1.2task |1.3task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,16 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+
+  let startValue = 1;
+
+  for (let i = 0; i < n; i++) {
+    startValue = startValue * (n - i);
+  }
+  return startValue;
 }
+
+factorial(5);
+factorial(3);
+factorial(1);
+factorial(0);
